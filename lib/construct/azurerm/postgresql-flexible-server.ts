972bf1109bcc4e3c90e1e12066f79e313e6f8514
ks6088ts <ks6088ts@gmail.com>
2025-10-31 14:08:00 +0900

    add PostgreSQL Flexible Server

---
+++ lib/construct/azurerm/postgresql-flexible-server.ts
@@ -0,0 +1,44 @@
+import { Construct } from 'constructs';
+import { postgresqlFlexibleServer } from '@cdktf/provider-azurerm';
+
+export interface PostgresqlFlexibleServerProps {
+  name: string;
+  location: string;
+  tags?: { [key: string]: string };
+  resourceGroupName: string;
+  skuName: string;
+  administratorLogin: string;
+  administratorPassword: string;
+  version: string;
+}
+
+export class PostgresqlFlexibleServer extends Construct {
+  public readonly postgresqlFlexibleServer: postgresqlFlexibleServer.PostgresqlFlexibleServer;
+  constructor(
+    scope: Construct,
+    id: string,
+    props: PostgresqlFlexibleServerProps,
+  ) {
+    super(scope, id);
+
+    // Resources
+
+    // PostgreSQL Flexible Server: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/postgresql_flexible_server
+    this.postgresqlFlexibleServer =
+      new postgresqlFlexibleServer.PostgresqlFlexibleServer(
+        this,
+        'postgresql_flexible_server',
+        {
+          name: props.name,
+          location: props.location,
+          tags: props.tags,
+          resourceGroupName: props.resourceGroupName,
+          skuName: props.skuName,
+          administratorLogin: props.administratorLogin,
+          administratorPassword: props.administratorPassword,
+          version: props.version,
+          zone: '2',
+        },
+      );
+  }
+}
